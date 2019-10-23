e40de019bf36b8cc4aff0361005a2d6374685903
Blake Olinger <blake.olinger@symbosoft.com>
2019-10-23 08:32:12 -0400

    Add project files.

---
+++ BasicRebuild/Program.cs
@@ -0,0 +1,22 @@
+﻿using SldWorks;
+using System.Threading;
+
+namespace BasicRebuild
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var swInstance = new SldWorks.SldWorks();
+            var model = (ModelDoc2)swInstance.ActiveDoc;
+
+            model.ForceRebuild3(false);
+
+            var delay = 300;
+
+            Thread.Sleep(delay);
+
+            model.ForceRebuild3(false);
+        }
+    }
+}
